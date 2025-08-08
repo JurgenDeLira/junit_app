fd604b33587269c8b597118396e8761617cd45b6
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-08-07 19:43:44 -0700

    4

--- src/main/java/org/jdelira/junitapp/models/Cuenta.java
+++ src/main/java/org/jdelira/junitapp/models/Cuenta.java
@@ -27,6 +27,16 @@ public class Cuenta {
         this.saldo = saldo;
     }
 
+
+
+    public void debito(BigDecimal monto){
+        this.saldo = this.saldo.subtract(monto);
+    }
+
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
+    }
+
     @Override
     public boolean equals(Object obj){
         if (obj == null || !(obj instanceof Cuenta)){
--- src/test/java/org/jdelira/junitapp/models/CuentaTest.java
+++ src/test/java/org/jdelira/junitapp/models/CuentaTest.java
@@ -14,18 +14,18 @@ class CuentaTest {
         //cuenta.setPersona("Jorge");
         String esperado = "Jorge";
         String real = cuenta.getPersona();
+        assertNotNull(real);
         assertEquals(esperado, real);
         assertTrue(real.equals("Jorge"));
-
     }
 
     @Test
     void testSaldoCuenta() {
         Cuenta cuenta = new Cuenta("Jorge", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
-
     }
 
     @Test
@@ -35,7 +35,23 @@ class CuentaTest {
 
         //assertNotEquals(cuenta2, cuenta);
         assertEquals(cuenta2, cuenta);
+    }
 
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Jorge", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
 
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Jorge", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
     }
 }
